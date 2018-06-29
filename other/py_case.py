8c4f5c002086fd3dce79c77efa6da2c3a1725f68
curve <heiyangs@163.com>
2018-06-29 22:49:03 +0800

    update

---
+++ other/py_case.py
@@ -0,0 +1,8 @@
+print("Hello world!")
+print("What's your name?")
+my_name = input()
+print("It is good to meet you, " + my_name)
+print(len(my_name))
+print("what is your age?")
+10
+print("You will be " + str(int(my_age) + 1) + " in a year.")
